da905ef0a97dbc5a7c3af5abbd2cfd3a88e0a1b6
Aluno <Aluno@ALU22204.educa.sf>
2020-01-08 21:44:48 -0300

    Projeto Vetor_11

---
+++ 11_Vetor/src/controller/ExemploVetor.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class ExemploVetor {
+
+	public static void main(String[] args) {
+		int numeros[] =new int[4];
+		numeros[0] = 6;
+		numeros[1] = 17;
+		System.out.println("numeros [2]="+ numeros[1]);
+
+	}
+
+}
