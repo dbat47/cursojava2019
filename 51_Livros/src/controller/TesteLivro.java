408932801c81dd869b31c26b8f80d0b5697cb2d2
Aluno <Aluno@ALU22204.educa.sf>
2020-02-03 21:29:34 -0300

    Cadastro de livro

---
+++ 51_Livros/src/controller/TesteLivro.java
@@ -0,0 +1,20 @@
+package controller;
+import model.Livro;
+import util.Teclado;
+public class TesteLivro {
+
+	public static void main(String[] args) {
+		
+		
+		Livro livro= new Livro();
+
+		
+		
+		livro.setIsbn(1254784598523L);
+	livro.setTitulo("");	
+		
+		
+		
+	}
+
+}
---
+++ 51_Livros/src/model/Livro.java
@@ -0,0 +1,81 @@
+package model;
+
+public class Livro {
+
+	
+	private long isbn;
+	private String titulo;
+	private String edicao;
+	private int ano;
+	private String formato;
+	private int paginas;
+	private String editora;
+	private String autor;
+	
+	
+	
+	public void setIsbn(long isbn) {
+		this.isbn=isbn;
+	}
+	
+	public long getIsbn() {
+		return isbn;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo=titulo;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setEdicao(String edicao) {
+		this.edicao=edicao;
+		
+	}
+	
+	public String getEdicao() {
+		return edicao;
+	}
+	public void setAno(int ano ) {
+		this.ano=ano;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public void setFormato(String formato) {
+		this.formato=formato;
+		
+	}
+
+	public Livro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Livro(long isbn, String titulo, String edicao, int ano, String formato, int paginas, String editora,
+			String autor) {
+		super();
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.edicao = edicao;
+		this.ano = ano;
+		this.formato = formato;
+		this.paginas = paginas;
+		this.editora = editora;
+		this.autor = autor;
+	}
+
+	@Override
+	public String toString() {
+		return "Livro [isbn=" + isbn + ", titulo=" + titulo + ", edicao=" + edicao + ", ano=" + ano + ", formato="
+				+ formato + ", paginas=" + paginas + ", editora=" + editora + ", autor=" + autor + ", getIsbn()="
+				+ getIsbn() + ", getTitulo()=" + getTitulo() + ", getEdicao()=" + getEdicao() + ", getAno()=" + getAno()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	
+	
+}
