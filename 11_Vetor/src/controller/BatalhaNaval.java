7d0408b41cda046ebffeeebe2d64668fefe2ebce
Aluno <Aluno@ALU22204.educa.sf>
2020-02-12 19:59:12 -0300

    gcghc

--- 11_Vetor/src/controller/BatalhaNaval.java
+++ 11_Vetor/src/controller/BatalhaNaval.java
@@ -1,14 +1,26 @@
 package controller;
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+
 import util.Teclado;
 public class BatalhaNaval {
 
+	
+	URL som= BatalhaNaval.class.getResource("som.wav");
+	AudioClip Som=Applet.newAudioClip(som);
+	
 	public static void main(String[] args) {
-		// "[0]" linha "[1]" coluna 
+		// "[0=primeiro]" linha "[1=segundo]" coluna 
 		
 		String arena [] [] = new String [4] [5];
-		 
-		  for (int i = 0; i<4; i++) {
-			  for (int j = 0; j < 5; j++) {
+		int a = 1;
+		
+
+		while(a<=5){
+		
+		  for (int i = 0; i<4;i++) {
+			  for (int j = 0; j < 5; j++ ) {
 				  arena [i] [j] = "água";
 			  }
 		  }
@@ -22,8 +34,18 @@ public class BatalhaNaval {
 		
 		linha= Teclado.lerInt("Digite a coordenada da linha:");
 		coluna= Teclado.lerInt("Digite a coordenada da coluna:");
-		System.out.println("Você Acertou:"+ arena[linha][coluna]);
+		
+	BatalhaNaval tocar= new BatalhaNaval();
+	tocar.Som.play();
+		
+		javax.swing.JOptionPane.showMessageDialog(null, "voce acertou:"+ arena[linha][coluna]);
+		
+		
+	
+	a=a+a++;
 		}
+	
+	}
 	}
 
 
---
+++ 51_Livros/readme.md
@@ -0,0 +1,2 @@
+este é um arquivo de readme.md
+este arquivo orientara como funciona o sistema 
